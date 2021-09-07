0c5adaebccf5bb2c986072e7d2ab20e9c7651551
William Piers <william.piers@obeo.fr>
2021-09-07 16:58:10 +0200

    [#23] Document extension point
    Change-Id: Ic939bbda7c5bd012f20397aa2d23ae11a90caef8

--- plugins/org.eclipse.poosl.rotalumisclient/src/org/eclipse/poosl/rotalumisclient/extension/IPooslDebugInformer.java
+++ plugins/org.eclipse.poosl.rotalumisclient/src/org/eclipse/poosl/rotalumisclient/extension/IPooslDebugInformer.java
@@ -20,11 +20,38 @@ package org.eclipse.poosl.rotalumisclient.extension;
  *
  */
 public interface IPooslDebugInformer {
+
+    /**
+     * Notifies the extension that it has been activated.
+     * 
+     * @param object
+     *            the selected object in the debug tree.
+     * @param message
+     *            the debug event message
+     */
     void debugSelectionChanged(ExternDebugItem object, ExternDebugMessage message);
 
+    /**
+     * Notifies that a debugging session ended.
+     * 
+     * @param launchID
+     *            the session ID
+     */
     void launchStopped(String launchID);
 
+    /**
+     * Notifies of an event during a debugging session.
+     * 
+     * @param lastMessage
+     *            the debug event message
+     */
     void lastMessageChanged(ExternDebugMessage lastMessage);
 
+    /**
+     * Notifies that a debugging session started.
+     * 
+     * @param object
+     *            the start message
+     */
     void launchStart(ExternLaunchStartMessage object);
 }
